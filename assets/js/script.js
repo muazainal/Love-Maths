0e7a8646725fc85d5903f46ee41b807f8fe35a2a
muazainal <muazainal@live.com>
2026-01-13 20:16:28 +0000

    adding fundtions into script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calsulateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
