3e72127bc4584cbb9653b8c8ac14614e24ca5e52
muazainal <muazainal@live.com>
2026-01-17 05:33:36 +0000

    Division game - try

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,10 +43,13 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
-  } else {
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
+    else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
   }
+}
 }
 
 /**
@@ -88,12 +91,14 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
-  } else {
+  } else if (operator === "÷") {
+	  return [operand1 / operand2, "division"];
+	  else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
   }
 }
-
+}
 /**
  * Gets the current score from the DOM and increments it by 1
  */
@@ -128,4 +133,10 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+	document.getElementById("operand1").textContent = operand1;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "÷";
 }
