649b0482f82434e0ccf56360c4d2fbe28ec9b0cb
muazainal <muazainal@live.com>
2026-01-17 14:37:06 +0000

    change the division symbol

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,7 +91,7 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
-  } else if (operator === "÷") {
+  } else if (operator === "/") {
 	  return [operand1 / operand2, "division"];
 	} else {
     alert(`Unimplemented operator ${operator}`);
@@ -139,5 +139,5 @@ function displayDivisionQuestion(operand1, operand2) {
   operand1 = operand1*operand2;
 	document.getElementById("operand1").textContent = operand1; 
 	document.getElementById("operand2").textContent = operand2;
-	document.getElementById("operator").textContent = "÷";
+	document.getElementById("operator").textContent = "/";
 }
